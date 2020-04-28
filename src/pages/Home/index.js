9994ce626d694a953b417c76f754ba0a97808924
Patrick <patrickperotti0@gmail.com>
2020-04-28 13:31:02 -0300

    home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$99,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$99,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$99,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$99,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$99,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$99,00</span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
