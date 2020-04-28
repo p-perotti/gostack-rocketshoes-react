06934a956218874ad83e398e99e68e3b37e38faa
Patrick <patrickperotti0@gmail.com>
2020-04-28 14:37:47 -0300

    api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$99,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$99,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$99,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$99,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$99,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=544xhttps://static.netshoes.com.br/produtos/tenis-converse-all-star-ct-as-core-ox/80/D26-0147-080/D26-0147-080_zoom1.jpg?ts=1587555358&?ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis</strong>
-        <span>R$99,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
